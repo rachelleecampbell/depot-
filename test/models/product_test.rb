9e31f5a358685cc28e9a141de094ba0944707392
Rachel Lee Campbell <rachelleecampbell@gmail.com>
2015-03-29 14:38:41 -0500

    added validations to products

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "product attributes must not be empty" do  
+    product = Product.new(title: "My Book Title",
+    description: "yyy",
+    image_url: "zzz.jpg")
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+    product.errors[:price]
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+    product.errors[:price]
+    product.price = 1
+    assert product.valid?
+end
+  
 end
