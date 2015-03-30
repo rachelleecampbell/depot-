51f55d31fd8b0cd8b04e8ab9830b2b0cf8118eb1
Rachel Lee Campbell <rachelleecampbell@gmail.com>
2015-03-30 15:04:38 -0500

    Validations

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -2,20 +2,14 @@ require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
 
-  test "product attributes must not be empty" do  
-    product = Product.new(title: "My Book Title",
-    description: "yyy",
-    image_url: "zzz.jpg")
-    product.price = -1
-    assert product.invalid?
-    assert_equal ["must be greater than or equal to 0.01"],
-    product.errors[:price]
-    product.price = 0
-    assert product.invalid?
-    assert_equal ["must be greater than or equal to 0.01"],
-    product.errors[:price]
-    product.price = 1
-    assert product.valid?
+test "product is not valid without a unique title - i18n" do
+    product = Product.new(title: products(:ruby).title,
+            description: "yyy",
+            price: 1,
+            image_url: "fred.gif")
+assert product.invalid?
+assert_equal [I18n.translate('errors.messages.taken')],
+product.errors[:title]
 end
   
 end
