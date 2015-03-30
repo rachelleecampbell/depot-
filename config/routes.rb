95817b39706b289eab29d178e162c4c5324d704d
Rachel Lee Campbell <rachelleecampbell@gmail.com>
2015-03-30 17:13:34 -0500

    Scaffold store and styled

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
-Rails.application.routes.draw do
-  resources :products
+Depot::Application.routes.draw do
+get "store/index"
+resources :products
+# The priority is based upon order of creation:
+# first created -> highest priority.
+# See how all your routes lay out with "rake routes".
+# You can have the root of your site routed with "root"
+root 'store#index', as: 'store'
+# ...
+end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -55,4 +63,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
